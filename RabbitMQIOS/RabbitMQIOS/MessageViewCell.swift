2e0247fcc20215a2b544bd2d460b60dd2b10cd5d
Thien Vu <thien.vu@softfoundry.com>
2022-09-22 00:25:24 +0700

    Add project for support RabbitMQ send Data

---
+++ RabbitMQIOS/RabbitMQIOS/MessageViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  MessageViewCell.swift
+//  RabbitMQIOS
+//
+//  Created by Mac Mini 2021_1 on 21/09/2022.
+//
+
+import Foundation
---
+++ RabbitMQIOS/RabbitMQIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RabbitMQIOS
+//
+//  Created by Mac Mini 2021_1 on 21/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
